15698c9240cd2bc06afb9eddb32e1a2c5cd82676
Raynor Chavez <raynorkirksonchavez@gmail.com>
2024-07-16 08:24:47 +0800

    improve resource and datasource tests

--- internal/provider/indices_datasource_test.go
+++ internal/provider/indices_datasource_test.go
@@ -3,7 +3,7 @@ package provider
 import (
 	"testing"
 
-	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
+	"github.com/hashicorp/terraform-plugin-testing/helper/resource"
 )
 
 func TestAccDataSourceIndices(t *testing.T) {
@@ -11,11 +11,25 @@ func TestAccDataSourceIndices(t *testing.T) {
 		PreCheck:                 func() { testAccPreCheck(t) },
 		ProtoV6ProviderFactories: testAccProtoV6ProviderFactories,
 		Steps: []resource.TestStep{
+			// Create an index to ensure we have data to read
+			{
+				Config: testAccIndexResourceConfig,
+				Check: resource.ComposeTestCheckFunc(
+					resource.TestCheckResourceAttr("marqo_index.test", "index_name", "example_index_3"),
+				),
+			},
+			// Read indices
 			{
 				Config: testAccDataSourceIndicesConfig,
 				Check: resource.ComposeTestCheckFunc(
 					resource.TestCheckResourceAttrSet("data.marqo_read_indices.test", "items.#"),
-					// Add more checks  based on data source schema
+					resource.TestCheckResourceAttrSet("data.marqo_read_indices.test", "items.0.index_name"),
+					resource.TestCheckResourceAttr("data.marqo_read_indices.test", "items.0.type", "unstructured"),
+					resource.TestCheckResourceAttr("data.marqo_read_indices.test", "items.0.vector_numeric_type", "float"),
+					resource.TestCheckResourceAttr("data.marqo_read_indices.test", "items.0.treat_urls_and_pointers_as_images", "true"),
+					resource.TestCheckResourceAttr("data.marqo_read_indices.test", "items.0.model", "open_clip/ViT-L-14/laion2b_s32b_b82k"),
+					resource.TestCheckResourceAttr("data.marqo_read_indices.test", "items.0.normalize_embeddings", "true"),
+					resource.TestCheckResourceAttr("data.marqo_read_indices.test", "items.0.inference_type", "marqo.CPU.small"),
 				),
 			},
 		},
@@ -24,4 +38,37 @@ func TestAccDataSourceIndices(t *testing.T) {
 
 const testAccDataSourceIndicesConfig = `
 data "marqo_read_indices" "test" {}
+`
+
+const testAccIndexResourceConfig = `
+resource "marqo_index" "test" {
+  index_name = "example_index_3"
+  settings = {
+    type = "unstructured"
+    vector_numeric_type = "float"
+    treat_urls_and_pointers_as_images = true
+    model = "open_clip/ViT-L-14/laion2b_s32b_b82k"
+    normalize_embeddings = true
+    inference_type = "marqo.CPU.small"
+    all_fields = []
+    number_of_inferences = 1
+    number_of_replicas = 0
+    number_of_shards = 1
+    storage_class = "marqo.basic"
+    text_preprocessing = {
+      split_length = 2
+      split_method = "sentence"
+      split_overlap = 0
+    }
+    image_preprocessing = {}
+    ann_parameters = {
+      space_type = "prenormalized-angular"
+      parameters = {
+        ef_construction = 512
+        m = 16
+      }
+    }
+    filter_string_max_length = 20
+  }
+}
 `
--- internal/provider/indices_resource_test.go
+++ internal/provider/indices_resource_test.go
@@ -1,6 +1,7 @@
 package provider
 
 import (
+	"fmt"
 	"os"
 	"testing"
 
@@ -20,22 +21,53 @@ func TestAccResourceIndex(t *testing.T) {
 		PreCheck:                 func() { testAccPreCheck(t) },
 		ProtoV6ProviderFactories: testAccProtoV6ProviderFactories,
 		Steps: []resource.TestStep{
+			// Create and Read testing
 			{
-				Config: testAccResourceIndexConfig,
+				Config: testAccResourceIndexConfig("example_index_3"),
 				Check: resource.ComposeTestCheckFunc(
-					resource.TestCheckResourceAttr("marqo_index.test", "index_name", "test_index"),
+					resource.TestCheckResourceAttr("marqo_index.test", "index_name", "example_index_3"),
 					resource.TestCheckResourceAttr("marqo_index.test", "settings.type", "unstructured"),
-					// Add more checks based on resource schema
+					resource.TestCheckResourceAttr("marqo_index.test", "settings.vector_numeric_type", "float"),
+					resource.TestCheckResourceAttr("marqo_index.test", "settings.treat_urls_and_pointers_as_images", "true"),
+					resource.TestCheckResourceAttr("marqo_index.test", "settings.model", "open_clip/ViT-L-14/laion2b_s32b_b82k"),
+					resource.TestCheckResourceAttr("marqo_index.test", "settings.normalize_embeddings", "true"),
+					resource.TestCheckResourceAttr("marqo_index.test", "settings.inference_type", "marqo.CPU.small"),
+					resource.TestCheckResourceAttr("marqo_index.test", "settings.number_of_inferences", "1"),
+					resource.TestCheckResourceAttr("marqo_index.test", "settings.number_of_replicas", "0"),
+					resource.TestCheckResourceAttr("marqo_index.test", "settings.number_of_shards", "1"),
+					resource.TestCheckResourceAttr("marqo_index.test", "settings.storage_class", "marqo.basic"),
+					resource.TestCheckResourceAttr("marqo_index.test", "settings.text_preprocessing.split_length", "2"),
+					resource.TestCheckResourceAttr("marqo_index.test", "settings.text_preprocessing.split_method", "sentence"),
+					resource.TestCheckResourceAttr("marqo_index.test", "settings.text_preprocessing.split_overlap", "0"),
+					resource.TestCheckResourceAttr("marqo_index.test", "settings.ann_parameters.space_type", "prenormalized-angular"),
+					resource.TestCheckResourceAttr("marqo_index.test", "settings.ann_parameters.parameters.ef_construction", "512"),
+					resource.TestCheckResourceAttr("marqo_index.test", "settings.ann_parameters.parameters.m", "16"),
+					resource.TestCheckResourceAttr("marqo_index.test", "settings.filter_string_max_length", "20"),
 				),
 			},
-			// add more steps to test updates
+			// ImportState testing
+			{
+				ResourceName:      "marqo_index.test",
+				ImportState:       true,
+				ImportStateVerify: true,
+			},
+			// Update and Read testing
+			{
+				Config: testAccResourceIndexConfig("example_index_3_updated"),
+				Check: resource.ComposeTestCheckFunc(
+					resource.TestCheckResourceAttr("marqo_index.test", "index_name", "example_index_3_updated"),
+					resource.TestCheckResourceAttr("marqo_index.test", "settings.inference_type", "marqo.CPU.large"),
+				),
+			},
+			// Delete testing automatically occurs in TestCase
 		},
 	})
 }
 
-const testAccResourceIndexConfig = `
+func testAccResourceIndexConfig(name string) string {
+	return fmt.Sprintf(`
 resource "marqo_index" "test" {
-  index_name = "test_index"
+  index_name = "%s"
   settings = {
     type = "unstructured"
     vector_numeric_type = "float"
@@ -43,6 +75,26 @@ resource "marqo_index" "test" {
     model = "open_clip/ViT-L-14/laion2b_s32b_b82k"
     normalize_embeddings = true
     inference_type = "marqo.CPU.small"
+    all_fields = []
+    number_of_inferences = 1
+    number_of_replicas = 0
+    number_of_shards = 1
+    storage_class = "marqo.basic"
+    text_preprocessing = {
+      split_length = 2
+      split_method = "sentence"
+      split_overlap = 0
+    }
+    image_preprocessing = {}
+    ann_parameters = {
+      space_type = "prenormalized-angular"
+      parameters = {
+        ef_construction = 512
+        m = 16
+      }
+    }
+    filter_string_max_length = 20
   }
 }
-`
+`, name)
+}
