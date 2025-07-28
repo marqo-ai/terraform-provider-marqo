9622ff7d82f460fd3f761518ab056158c1227ad5
keshavjois <keshava@marqo.ai>
2025-07-28 16:28:54 +1000

    Fixing linting

--- internal/provider/indices_datasource.go
+++ internal/provider/indices_datasource.go
@@ -510,11 +510,11 @@ func (d *indicesDataSource) Read(ctx context.Context, req datasource.ReadRequest
 	}
 
 	storaceClassMap := map[string]string{
-		"BASIC":       "marqo.basic",
-		"BALANCED":    "marqo.balanced",
+		"BASIC":               "marqo.basic",
+		"BALANCED":            "marqo.balanced",
 		"BALANCED_STORAGE":    "marqo.balanced.storage",
-		"BALANCED_THROUGHPUT":    "marqo.balanced.throughput",
-		"PERFORMANCE": "marqo.performance",
+		"BALANCED_THROUGHPUT": "marqo.balanced.throughput",
+		"PERFORMANCE":         "marqo.performance",
 	}
 
 	items := make([]indexModel, len(indices))
--- internal/provider/indices_resource.go
+++ internal/provider/indices_resource.go
@@ -642,11 +642,11 @@ func (r *indicesResource) Read(ctx context.Context, req resource.ReadRequest, re
 		}
 
 		storaceClassMap := map[string]string{
-			"BASIC":       "marqo.basic",
-			"BALANCED":    "marqo.balanced",
+			"BASIC":               "marqo.basic",
+			"BALANCED":            "marqo.balanced",
 			"BALANCED_STORAGE":    "marqo.balanced.storage",
-			"BALANCED_THROUGHPUT":    "marqo.balanced.throughput",
-			"PERFORMANCE": "marqo.performance",
+			"BALANCED_THROUGHPUT": "marqo.balanced.throughput",
+			"PERFORMANCE":         "marqo.performance",
 		}
 
 		if !newState.Settings.InferenceType.IsNull() {
@@ -1511,16 +1511,16 @@ func (r *indicesResource) Update(ctx context.Context, req resource.UpdateRequest
 
 	// Map storage class values similar to inference type
 	storageClassMap := map[string]string{
-		"BASIC":             "marqo.basic",
-		"BALANCED":          "marqo.balanced",
+		"BASIC":                     "marqo.basic",
+		"BALANCED":                  "marqo.balanced",
 		"BALANCED_STORAGE":          "marqo.balanced.storage",
-		"BALANCED_THROUGHPUT":          "marqo.balanced.throughput",
-		"PERFORMANCE":       "marqo.performance",
-		"marqo.basic":       "marqo.basic",
-		"marqo.balanced":    "marqo.balanced",
+		"BALANCED_THROUGHPUT":       "marqo.balanced.throughput",
+		"PERFORMANCE":               "marqo.performance",
+		"marqo.basic":               "marqo.basic",
+		"marqo.balanced":            "marqo.balanced",
 		"marqo.balanced.storage":    "marqo.balanced.storage",
-		"marqo.balanced.throughput":    "marqo.balanced.throughput",
-		"marqo.performance": "marqo.performance",
+		"marqo.balanced.throughput": "marqo.balanced.throughput",
+		"marqo.performance":         "marqo.performance",
 	}
 
 	mappedStorageClass := currentIndex.StorageClass
